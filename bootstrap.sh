deeaaacb4e8a96f75ba1e51cc0a9d66c15a24cc7
Adam Skrzypulec <adam.s@northwestern.edu>
2016-03-15 15:13:22 -0500

    minor whitespace cleanup

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,24 +5,22 @@ yum update
 yum install -y httpd postgresql94-server
 chkconfig httpd on
 
-#install PHP including  modules
+#install PHP including modules
 yum install -y httpd php-devel php-snmp php-xml php-xmlrpc php-soap php-ldap php-pgsql php-mcrypt php-mbstring php-gd php-tidy php-pspell php-pecl-memcache
 
 PGDIR="/var/lib/pgsql/data"
-if [ "$(ls -A $PGDIR)" ]; then
-     echo $PGSIR "not empty"
-else
-	echo $PGSIR "initializing"
-    service postgresql initdb --locale en_US.UTF-8 --pwfile=/vagrant/etc/pgsql/pg_pw
-    chkconfig postgresql on
-    cp /vagrant/etc/pgsql/pg_hba.conf /var/lib/pgsql/data/
+if ! [ "$(ls -A $PGDIR)" ]; then
+	echo $PGSIR "Initializing postgres db"
+	service postgresql initdb --locale en_US.UTF-8 --pwfile=/vagrant/etc/pgsql/pg_pw
+	chkconfig postgresql on
+	cp /vagrant/etc/pgsql/pg_hba.conf /var/lib/pgsql/data/
 fi
 service postgresql start
 
 # Setup the web server's root directory
 if ! [ -L /var/www ]; then
-  rm -rf /var/www
-  ln -fs /vagrant /var/www
+	rm -rf /var/www
+	ln -fs /vagrant /var/www
 fi
 if ! [ -d /vagrant/html ]; then
 	mkdir /vagrant/html
@@ -34,11 +32,11 @@ cp /vagrant/etc/httpd/conf/httpd.conf /etc/httpd/conf/httpd.conf
 service httpd start
 
 if [ ! -f /usr/local/bin/composer ]; then
-  # download the latest composer.phar version
-  curl -s https://getcomposer.org/installer | php
+	# download the latest composer.phar version
+	curl -s https://getcomposer.org/installer | php
 
-  # Move to bin path
-  mv composer.phar /usr/local/bin/composer
+	# Move to bin path
+	mv composer.phar /usr/local/bin/composer
 else
-  composer self-update
+	/usr/local/bin/composer self-update
 fi
