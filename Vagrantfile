398b96fdeb13f8d8ad3388d0973f583358688f50
Adam Skrzypulec <adam.s@northwestern.edu>
2016-03-17 17:02:37 -0500

    changed the postgres configuration and the apache port

--- Vagrantfile
+++ Vagrantfile
@@ -70,6 +70,6 @@ Vagrant.configure(2) do |config|
   # SHELL
 
   config.vm.provision :shell, path: "init.sh"
-  config.vm.network :forwarded_port, guest: 80, host: 4567
+  config.vm.network :forwarded_port, guest: 80, host: 8080
 
 end
--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Update the system and install the servers
-yum install -y httpd postgresql94-server
+yum install -y httpd postgresql-server
 chkconfig httpd on
 
 #install PHP including modules
@@ -10,23 +10,25 @@ yum install -y httpd php-devel php-snmp php-xml php-xmlrpc php-soap php-ldap php
 # use use a custom PHP configuration
 cp /vagrant/etc/php.ini /etc
 
-# isntall the PostgeSQL database
+# install the PostgeSQL database
 PGDIR="/var/lib/pgsql/data"
-if ! [ "$(ls -A $PGDIR)" ]; then
-	echo $PGSIR "Initializing postgres db"
-	service postgresql initdb --locale en_US.UTF-8 --pwfile=/vagrant/etc/pgsql/pg_pw
-	chkconfig postgresql on
-	cp /vagrant/etc/pgsql/pg_hba.conf /var/lib/pgsql/data/
+if ! [ -d $PGDIR ] || ! [ "$(ls -A $PGDIR)" ]; then
+mkdir -p $PGDIR
+chown postgres:postgres $PGDIR
+echo "Setting up postgresql in $PGDIR"
+service postgresql initdb --locale en_US.UTF-8 --pwfile=/vagrant/etc/pgsql/pg_pw
+chkconfig postgresql on
+cp /vagrant/etc/pgsql/pg_hba.conf /var/lib/pgsql/data/
 fi
 service postgresql start
 
 # Setup the web server's root directory
 if ! [ -L /var/www ]; then
-	rm -rf /var/www
-	ln -fs /vagrant /var/www
+rm -rf /var/www
+ln -fs /vagrant /var/www
 fi
 if ! [ -d /vagrant/html ]; then
-	mkdir /vagrant/html
+mkdir /vagrant/html
 fi
 
 # Prevent the Apache warning "Could not reliably determine the server's fully qualified domain name"
@@ -35,13 +37,13 @@ cp /vagrant/etc/httpd/conf/httpd.conf /etc/httpd/conf/httpd.conf
 service httpd start
 
 if [ ! -f /usr/local/bin/composer ]; then
-	# download the latest composer.phar version
-	curl -s https://getcomposer.org/installer | php
+# Download the latest composer.phar version
+curl -s https://getcomposer.org/installer | php
 
-	# Move to bin path
-	mv composer.phar /usr/local/bin/composer
+# Move to bin path
+mv composer.phar /usr/local/bin/composer
 else
-	/usr/local/bin/composer self-update
+/usr/local/bin/composer --quiet self-update
 fi
 
 # Custom environment variables, aliases & paths
