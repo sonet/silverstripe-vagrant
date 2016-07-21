ca848da11fc02673cec43ba2f958ef0e7f669690
Adam Skrzypulec <adam.s@northwestern.edu>
2016-07-21 14:12:43 -0500

    Minor vagrant config file cleanup.

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Install the CenrOS Software Collection (SCL)
-yum install centos-release-scl
+yum install -y centos-release-scl
 
 # Add the Remi repository
 wget http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
@@ -12,25 +12,35 @@ cp /vagrant/etc/yum.repos.d/remi.repo /etc/yum.repos.d/
 
 # Accept the EPEL gpg key
 rpm --import http://download.fedoraproject.org/pub/epel/RPM-GPG-KEY-EPEL-6
-yum -y install epel-release
+yum install -y epel-release
 
 # Install the EPEL Node.js
-yum -y install nodejs npm --enablerepo=epel
+yum install -y nodejs npm --enablerepo=epel
 
 # Install the servers including PHP w/ modules
-yum install -y httpd postgresql-server postgresql-contrib php php-pear php-devel php-snmp php-xml php-xmlrpc php-soap php-ldap php-pgsql php-mcrypt php-mbstring php-gd php-tidy php-pspell php-pecl-memcache php-tcpdf  --enablerepo=remi
+yum install -y httpd postgresql-server postgresql-contrib php php-pear php-devel php-snmp php-xml php-xmlrpc php-soap php-ldap php-pgsql php-mcrypt php-mbstring php-gd php-tidy php-pspell php-pecl-memcache php-tcpdf vim git --enablerepo=remi
 chkconfig httpd on
 
-# Install some useful development tools
-yum install -y vim git
-
 # use use a custom PHP configuration
-
 cp /vagrant/etc/php.ini /etc
 
 # make the php temporary writable to the web server user
 mkdir -p /var/lib/php/session/
 chown -R vagrant:vagrant /var/lib/php/session/
+
+# Setup the Apache web server
+cp /vagrant/etc/httpd/conf/httpd.conf /etc/httpd/conf/httpd.conf
+if ! [ -L /var/www ]; then
+rm -rf /var/www
+ln -fs /vagrant /var/www
+fi
+if ! [ -d /vagrant/html ]; then
+mkdir /vagrant/html
+fi
+if ! [ -d /vagrant/node ]; then
+mkdir /vagrant/node
+fi
+service httpd start
 
 # Setup the PostgeSQL database
 PGDIR="/var/lib/pgsql/data"
@@ -42,7 +52,6 @@ chown -R postgres:postgres /var/lib/pgsql/
 service postgresql initdb --locale en_US.UTF-8 -U postgres
 cp /vagrant/etc/pgsql/pg_hba.conf $PGDIR
 chown -R postgres:postgres /var/lib/pgsql/
-
 chkconfig postgresql on
 fi
 service postgresql start
@@ -53,25 +62,9 @@ cp /vagrant/etc/pgsql/postgresql.conf /var/lib/pgsql/data/postgresql.conf
 chown -R postgres:postgres /var/lib/pgsql/
 service postgresql restart
 
-# Setup the web servers root directory
-if ! [ -L /var/www ]; then
-rm -rf /var/www
-ln -fs /vagrant /var/www
-fi
-if ! [ -d /vagrant/html ]; then
-mkdir /vagrant/html
-fi
-if ! [ -d /vagrant/node ]; then
-mkdir /vagrant/node
-fi
-
-# Prevent the Apache warning "Could not reliably determine the server's fully qualified domain name"
-cp /vagrant/etc/httpd/conf/httpd.conf /etc/httpd/conf/httpd.conf
-
-if [ ! -f /usr/local/bin/composer ]; then
 # Download the latest composer.phar version
+if [ ! -f /usr/local/bin/composer ]; then
 curl -s https://getcomposer.org/installer | php
-
 # Move to bin path
 mv composer.phar /usr/local/bin/composer
 else
